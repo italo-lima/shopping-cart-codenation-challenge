0223227755277e2bdef6bacf62ca676993d401da
Ítalo Lima <italojonas@hotmail.com>
2020-10-17 20:33:56 -0300

    added json server

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,58 +1,49 @@
-import React, {useEffect, useState, useCallback} from "react"
+import React, { useEffect, useState, useCallback } from "react";
 
-import Loading from "../../components/Loading"
-import Card from "../../components/Card"
-import {
-  Container, 
-  Products, 
-  GridCatalog, 
-} from "./styles"
-import api from "../../services/api"
-import {convertToSlug} from "../../utils/convertSlug"
+import Loading from "../../components/Loading";
+import Card from "../../components/Card";
+import { Container, Products, GridCatalog } from "./styles";
+import api from "../../services/api";
+import { convertToSlug } from "../../utils/convertSlug";
 
-
-export default function Home(){
-
-  const [catalog, setCatalog] = useState([])
-  const [loadingCatalog, setLoadingCatalog] = useState(false)
-  const [countCatalog, setCountCatalog] = useState(0)
+export default function Home() {
+  const [catalog, setCatalog] = useState([]);
+  const [loadingCatalog, setLoadingCatalog] = useState(false);
+  const [countCatalog, setCountCatalog] = useState(0);
 
   const loadRepo = useCallback(async () => {
-    setLoadingCatalog(true)
-    const response = await api.get('/catalog')
-    setCountCatalog(response.data.length)
+    setLoadingCatalog(true);
+    const response = await api.get("/catalog");
+    setCountCatalog(response.data.length);
 
-    const data = response.data.map(element => ({
-        ...element,
-        slug: convertToSlug(element.name),
+    const data = response.data.map((element) => ({
+      ...element,
+      slug: convertToSlug(element.name),
     }));
 
-    setCatalog(data)
-    setLoadingCatalog(false)
-  }, []
-  )
-  
-  useEffect(() => {
-    loadRepo()
-  }, [loadRepo])
+    setCatalog(data);
+    setLoadingCatalog(false);
+  }, []);
 
-  return(
+  useEffect(() => {
+    loadRepo();
+  }, [loadRepo]);
+
+  return (
     <>
-    {console.log("state", loadingCatalog)}
       <Products>
         {loadingCatalog && <Loading />}
+
         <Container>
           <div>{countCatalog} itens</div>
           <GridCatalog>
-            {catalog && catalog.map(element => (
-              <Card 
-                key={element.code_color}
-                element={element}
-              />
-            ))}
+            {catalog &&
+              catalog.map((element) => (
+                <Card key={element.code_color} element={element} />
+              ))}
           </GridCatalog>
         </Container>
       </Products>
-      </>
-  )
+    </>
+  );
 }
